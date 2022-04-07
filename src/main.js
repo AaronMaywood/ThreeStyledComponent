a3d732ed3c89ad7497da0856dc6351544f25a1a9
Aaron Maywood <am7274183@gmail.com>
2022-04-07 11:18:30 +0900

    ページ用コンポーネントの名前をIndex.svelte とした

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
-import App from './App.svelte';
+import Index from './Index.svelte';
 
-const app = new App({
+const index = new Index({
 	target: document.body,
-	props: {
-		name: 'world'
-	}
 });
 
-export default app;
+export default index;
