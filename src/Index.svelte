9f08a652aaaa5bcece450af4605a57b116bcb753
Aaron Maywood <am7274183@gmail.com>
2022-04-07 14:10:30 +0900

    PageHeader の実装

--- src/Index.svelte
+++ src/Index.svelte
@@ -1,5 +1,7 @@
 <script>
+import PageHeader from './Lib/PageHeader.svelte';
 import Product from './Lib/Product.svelte';
 </script>
 
+<PageHeader />
 <Product />
---
+++ src/Lib/PageHeader.svelte
@@ -0,0 +1,17 @@
+<header><h1>The Brooklyn Garden</h1><div class="menu">☰</div></header> 
+
+<style>
+header {
+  grid-area: 😄;
+  background: #c2ceb0;
+  padding: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h1,
+.menu {
+  font-size: 1.5rem;
+}
+</style>
